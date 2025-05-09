0b74c2762154f211aa88b89a0c7b23926dfa512d
simon despres <simondprs62790@hotmail.fr>
2025-05-09 19:05:17 +0200

    comment the code and learn list in python

---
+++ test.py
@@ -0,0 +1,22 @@
+#guichet de cinéma
+#recolter l'age de la personne
+#si la personne est mineur -> 7€
+#si la personne est majeur -> 9€
+#souhaitez-vous du pop-corn -> 5€
+#affichez le prix
+
+#age = int(input("Bonjour, quel age avez-vous ? "))
+#total = 0
+
+#if age < 18:
+#    total += 7
+#else:
+#    total += 9
+
+#bool_popcorn = input("Souhaitez-vous du popcorn ? ")
+
+#if bool_popcorn == "oui":
+#    total += 5
+#    print("Cela fera {}€ s'il vous plait.".format(total))
+#else:
+#    print("Cela fera {}€ s'il vous plait.".format(total))
